086daf901330a7168cf8ace90161ba9ab3ef1bac
igorujevic <iujevi00@fesb.hr>
2020-11-26 18:21:53 +0100

    Add basic design for timeline and sidebars

---
+++ resources/views/_sidebar-links.blade.php
@@ -0,0 +1,26 @@
+<ul>
+  <li>
+    <a href="/" class="font-bold text-lg mb-4 black">Home</a>
+  </li>
+  <li>
+    <a href="/explore" class="font-bold text-lg mb-4 black">Explore</a>
+  </li>
+  <li>
+    <a href="/#" class="font-bold text-lg mb-4 black">Notifications</a>
+  </li>
+  <li>
+    <a href="/#" class="font-bold text-lg mb-4 black">Messages</a>
+  </li>
+  <li>
+    <a href="/#" class="font-bold text-lg mb-4 black">Bookmarks</a>
+  </li>
+  <li>
+    <a href="/#" class="font-bold text-lg mb-4 black">Lists</a>
+  </li>
+  <li>
+    <a href="/#" class="font-bold text-lg mb-4 black">Profile</a>
+  </li>
+  <li>
+    <a href="/#" class="font-bold text-lg mb-4 black">More</a>
+  </li>
+</ul>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,10 @@
+<div class="flex p-4 border-b border-b-gray-400">
+  <div class="mr-4 flex-shrink-0">
+    <img src="https://i.pravatar.cc/50" alt="avatar" class="rounded-full">
+
+  </div>
+  <div>
+    <h5 class="font-bold mb-4">John Doe</h5>
+    <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fuga vel fugiat pariatur animi ut?</p>
+  </div>
+</div>
