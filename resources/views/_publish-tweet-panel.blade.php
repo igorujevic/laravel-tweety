929aa34e483dcb14b36b01e4eeac63c847c5a410
igorujevic <iujevi00@fesb.hr>
2020-11-27 13:34:05 +0100

    Small fix: publish tweet profile page size

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -14,7 +14,9 @@
       <img
         src="{{ auth()->user()->avatar }}"
         alt="avatar"
-        class="rounded-full mr-2">
+        class="rounded-full mr-2"
+        width="50"
+        height="50">
       <button
         type="submit"
         class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">
